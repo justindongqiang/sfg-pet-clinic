2c6ca3fa53d84e578a5ee478dfb336750fc703ff
dongj <Justin Dong>
2020-05-10 16:33:13 -0500

    Implement pet clinic pojo data model #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,30 @@
+package guru.springframework.sfgpetclinic.model;
+
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Pet {
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
+    private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
+    @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthDate;
+
+//    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+//    private Set<Visit> visits = new HashSet<>();
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,27 @@
+package guru.springframework.sfgpetclinic.model;
+
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "types")
+public class PetType {
+
+
+    @Column(name = "name")
+    private String name;
+
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
