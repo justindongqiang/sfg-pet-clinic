abe8d7d8fb391877067fe1e8e6b6d4ee45961946
dongj <Justin Dong>
2020-05-10 16:00:16 -0500

    Smoke tests

--- src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,13 +1,20 @@
 package guru.springframework.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class SfgPetClinicApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
+    @Test
+    public void main() {
+        SfgPetClinicApplication.main(new String[] {});
+    }
 }
