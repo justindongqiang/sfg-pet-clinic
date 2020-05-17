2d8db74f0093aa7330305ac5270e9e6b9f681b4d
dongj <Justin Dong>
2020-05-16 21:29:01 -0500

    Added specialities, added specialities to vet.

--- src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -5,12 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Set;
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Vet extends Person{
-    private Set<Speciality> specialities;
+public class Speciality extends BaseEntity {
+
+    private String description;
+
 }
