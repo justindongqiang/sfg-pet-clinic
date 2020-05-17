3b0a2c7b8c3bbe17a61b92479fe920207b498fa5
dongj <Justin Dong>
2020-05-16 21:33:26 -0500

    added more properties to owner.

--- src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,4 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Owner extends Person{
+    private String address;
+    private String city;
+    private String telephone;
+    private Set<Pet> pets;
 }
