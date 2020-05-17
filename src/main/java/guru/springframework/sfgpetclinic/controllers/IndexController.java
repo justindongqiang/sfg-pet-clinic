3e6c70db8d1bc753f4d25743ff06ce5bcc524e2b
dongj <Justin Dong>
2020-05-17 16:30:51 -0500

    Fixed Broken Links.

--- src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -9,7 +9,11 @@ public class IndexController {
 
     @RequestMapping({"", "/", "index", "index.html"})
     public String index(){
-
         return "index";
+    }
+
+    @RequestMapping("/oups")
+    public String oupsHandler(){
+        return "notimplemented";
     }
 }
